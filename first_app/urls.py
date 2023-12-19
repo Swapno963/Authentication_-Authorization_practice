7bb2579bfa5649d84e55e99fec2049291dc7798e
swapno963 <swapno963@gmail.com>
2023-12-20 00:45:31 +0600

    login and signup done

--- first_app/urls.py
+++ first_app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.home, name ='home')
+    path('',views.home, name ='home'),
+    path('login/', views.user_login, name ='login'),
+    path('profile/', views.user_profile, name ='profile'),
+    path('signup/', views.user_signup, name ='signup'),
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -1,9 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import RegisterForm
 from django.contrib import messages
+
+# for login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login,logout
+
+
 # Create your views here.
 def home(request):
     print(request.POST)
+    return render(request,'home.html')
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data = request.POST)
+        if form.is_valid():
+            user_name = form.cleaned_data['username']
+            user_pass = form.cleaned_data['password']
+            user = authenticate(username=user_name, password=user_pass)
+            # print('user is :',user)
+            if user is not None:
+                    login(request, user)
+                    return redirect('profile')
+    else: 
+        form = AuthenticationForm()
+    return render(request, 'login.html',{'form':form})
+
+def user_profile(request):
+    # print('from profile',request)
+    return render(request, 'profile.html',{'user':request.user})
+
+def user_signup(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
@@ -14,4 +42,4 @@ def home(request):
             form.save()
     else:
         form = RegisterForm()
-    return render(request,'home.html', {'form':form})
+    return render(request, 'signup.html', {'form':form})
