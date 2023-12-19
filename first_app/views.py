b47816b876577e2bfaa9c9e054745d9dbf109ecc
swapno963 <swapno963@gmail.com>
2023-12-19 22:46:58 +0600

    user creation form with all credentials

--- first_app/views.py
+++ first_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .forms import RegisterForm
 # Create your views here.
+def home(request):
+    form = RegisterForm()
+    return render(request,'home.html', {'form':form})
