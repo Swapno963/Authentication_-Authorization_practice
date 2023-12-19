d11355a8d16c7a6bd0d147a12a547b2483d86663
swapno963 <swapno963@gmail.com>
2023-12-20 01:09:22 +0600

    authenticate user can not access login and sigup page

--- first_app/urls.py
+++ first_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('login/', views.user_login, name ='login'),
     path('profile/', views.user_profile, name ='profile'),
     path('signup/', views.user_signup, name ='signup'),
+    path('logout/', views.user_logout, name ='logout'),
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -13,33 +13,45 @@ def home(request):
     return render(request,'home.html')
 
 def user_login(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request=request, data = request.POST)
-        if form.is_valid():
-            user_name = form.cleaned_data['username']
-            user_pass = form.cleaned_data['password']
-            user = authenticate(username=user_name, password=user_pass)
-            # print('user is :',user)
-            if user is not None:
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = AuthenticationForm(request=request, data = request.POST)
+            if form.is_valid():
+                user_name = form.cleaned_data['username']
+                user_pass = form.cleaned_data['password']
+                user = authenticate(username=user_name, password=user_pass)
+                # print('user is :',user)
+                if user is not None:
                     login(request, user)
-                    return redirect('profile')
-    else: 
-        form = AuthenticationForm()
-    return render(request, 'login.html',{'form':form})
+                    return redirect('profile')    
+        else: 
+            form = AuthenticationForm()
+        return render(request, 'login.html',{'form':form})
+    else:
+        return redirect('profile')
 
 def user_profile(request):
-    # print('from profile',request)
-    return render(request, 'profile.html',{'user':request.user})
+    if request.user.is_authenticated:
+        return render(request, 'profile.html',{'user':request.user})
+    else:
+        return redirect('login')
 
 def user_signup(request):
-    if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            messages.success(request, 'Account Created Successfully')
-            messages.warning(request, 'Account Created Successfully, Warning')
-            messages.info(request, 'Account Created Successfully, Info')
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = RegisterForm(request.POST)
+            if form.is_valid():
+                messages.success(request, 'Account Created Successfully')
+                messages.warning(request, 'Account Created Successfully, Warning')
+                messages.info(request, 'Account Created Successfully, Info')
 
-            form.save()
+                form.save()
+        else:
+            form = RegisterForm()
+        return render(request, 'signup.html', {'form':form})
     else:
-        form = RegisterForm()
-    return render(request, 'signup.html', {'form':form})
+        return redirect('profile')
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
