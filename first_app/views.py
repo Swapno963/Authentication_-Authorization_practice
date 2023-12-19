16974d755804a91c0c5759f465885b9fe074911a
swapno963 <swapno963@gmail.com>
2023-12-19 23:53:03 +0600

    3 type of message given after creating user and thos text colored in diffrent way

--- first_app/views.py
+++ first_app/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from .forms import RegisterForm
+from django.contrib import messages
 # Create your views here.
 def home(request):
     print(request.POST)
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
-            form.save(commit=False)
+            messages.success(request, 'Account Created Successfully')
+            messages.warning(request, 'Account Created Successfully, Warning')
+            messages.info(request, 'Account Created Successfully, Info')
+
+            form.save()
     else:
         form = RegisterForm()
     return render(request,'home.html', {'form':form})
