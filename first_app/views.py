5faf387bad2cec0a18424f83d01b2f9381bad537
swapno963 <swapno963@gmail.com>
2023-12-19 23:32:43 +0600

    now first_name,last_name and email is required

--- first_app/views.py
+++ first_app/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from .forms import RegisterForm
 # Create your views here.
 def home(request):
-    form = RegisterForm()
+    print(request.POST)
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save(commit=False)
+    else:
+        form = RegisterForm()
     return render(request,'home.html', {'form':form})
